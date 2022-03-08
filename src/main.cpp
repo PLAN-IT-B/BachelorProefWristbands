a224e2e6b8935d44406d5f1a92186d428f6757a2
Blet-er <missiaen.robin@gmail.com>
2022-03-08 09:45:52 +0100

    ECHTE CODE voor 4 aespas

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,33 @@
 #include <BLECast.h>
 #include <string.h>
 
-#ifndef BEACON
 
     BLEScan *pBLEScan;
+    BLECast bleCast("esp1");
 
     const int scanTimeSeconds = 1;
+    uint8_t cnt = 0;
+    char data[5];
+    uint8_t mode = 2;
 
 
     class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
     {
         void onResult(BLEAdvertisedDevice advertisedDevice)
         {
-<<<<<<< HEAD
-            if (advertisedDevice.getName().compare("hknogroi39=")!=0)
-=======
-            if (advertisedDevice.getName().compare("Y2hvY29jaGVzbmV5") == 0)
->>>>>>> 2f817401dba18aad8460ecfd4c186b636b7f67b7
+            if (advertisedDevice.getName().compare("esp2") == 0)
+
             {
                 Serial.print(advertisedDevice.getName().c_str());
                 Serial.printf(": %d \n", advertisedDevice.getRSSI());
                 Serial.printf(": %s \n", advertisedDevice.getManufacturerData().c_str());
+                mode ++;
             }
         }
     };
 
-    void setup()
-    {
+    void scansetup(){
+
         Serial.begin(115200);
         Serial.println("Scanning...");
 
@@ -43,34 +44,20 @@
         pBLEScan->setWindow(99); // less or equal setInterval value
     }
 
-    void loop()
-    {
-        BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
-        pBLEScan->clearResults();
-    }
-#else
-    // define BTLE name
-    // CAREFUL: each character eats into your usable adv packet length
-<<<<<<< HEAD
-    BLECast bleCast("hknogroi39=");
-=======
-    BLECast bleCast("ESP32-WristbandV4.20");
->>>>>>> 2f817401dba18aad8460ecfd4c186b636b7f67b7
-    
-    uint8_t cnt = 0;
-    char data[5];
-
-    void setup()
-    {
+    void sendsetup(){
         Serial.begin(115200);
         Serial.println("Starting BLE Beacon");
 
         bleCast.begin();
     }
 
-    void loop()
-    {
-        // note -- if you have too much data, it will not be added to the adv payload
+    void scanloop(){
+        BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
+        pBLEScan->clearResults();
+    }
+
+    void sendloop(){
+                // note -- if you have too much data, it will not be added to the adv payload
 
         if (cnt == 20){
             // reset
@@ -88,5 +75,21 @@
         std::string s = bleCast.setManufacturerData(data, sizeof(data));
         Serial.println(s.c_str());
         delay(1000);
+        mode ++;
     }
-#endif
+
+    void setup()
+    {
+        scansetup();
+        sendsetup();
+    }
+
+
+
+    void loop()
+    {
+        if (mode%2 <= 1)
+            scanloop();
+        else
+            sendloop();
+    }
