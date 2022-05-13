0d8b8171ccb7a3c177c79fbeef3ec8e928ee7599
Blet-er <missiaen.robin@gmail.com>
2022-05-13 13:28:48 +0200

    updates

---
+++ Code/Reset-Code/src/main.cpp
@@ -0,0 +1,149 @@
+#include "WiFi.h"
+#include "PubSubClient.h" //pio lib install "knolleary/PubSubClient"
+
+#define SSID          "NETGEAR68"
+#define PWD           "excitedtuba713"
+
+#define MQTT_SERVER   "192.168.1.2"
+#define MQTT_PORT     1883
+
+#define LED_PIN       2
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void callback(char *topic, byte *message, unsigned int length);
+
+void setup_wifi()
+{
+  delay(10);
+  Serial.println("Connecting to WiFi..");
+
+  WiFi.begin(SSID, PWD);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void setup()
+{
+
+  Serial.begin(115200);
+
+  setup_wifi();
+  client.setServer(MQTT_SERVER, MQTT_PORT);
+  client.setCallback(callback);
+
+  
+
+  pinMode(LED_PIN, OUTPUT);
+  
+}
+
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.print("Message arrived on topic: ");
+  Serial.print(topic);
+  Serial.print(". Message: ");
+  String messageTemp;
+
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)message[i]);
+    messageTemp += (char)message[i];
+  }
+  Serial.println();
+
+  // Feel free to add more if statements to control more GPIOs with MQTT
+    // If a message is received on the topic esp32/output, you check if the message is either "on" or "off".
+    // Changes the output state according to the message
+    // if (String(topic) == "esp32/output")
+    // {
+    //   Serial.print("Changing output to ");
+    //   if (messageTemp == "on")
+    //   {
+    //     Serial.println("on");
+    //     digitalWrite(LED_PIN, HIGH);
+    //   }
+    //   else if (messageTemp == "off")
+    //   {
+    //     Serial.println("off");
+    //     digitalWrite(LED_PIN, LOW);
+    //   }
+    // }
+}
+
+void reconnect()
+{
+  // Loop until we're reconnected
+  while (!client.connected())
+  {
+    Serial.print("Attempting MQTT connection...");
+    // Attempt to connect
+    // CREATE UNIQUE client ID!
+    // in Mosquitto broker enable anom. access
+    if (client.connect("ESP8266Client"))
+    {
+      Serial.println("connected");
+      // Subscribe
+      // Vul hieronder in naar welke directories je gaat luisteren.
+      //Voor de communicatie tussen de puzzels, check "Datacommunicatie.docx". (terug tevinden in dezelfde repository) 
+      client.subscribe("controlpanel/reset");
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(5000);
+    }
+  }
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    reconnect();
+  }
+  client.loop();
+
+  long now = millis();
+  if (now - lastMsg > 5000)
+  {
+    lastMsg = now;
+  }
+
+  //client.publish("Wristbands/reset","Reset wristbands");
+  //client.publish("wristbands","Groen");
+  //digitalWrite(LED_PIN, HIGH);
+  //digitalWrite(LED_PIN, LOW);
+  //delay(10000);
+  //client.publish("wristbands","Oranje");
+  //digitalWrite(LED_PIN, HIGH);
+  //delay(1000);
+  //digitalWrite(LED_PIN, LOW);
+  //delay(100000000000);
+
+  //Hieronder kan je beginnen werken aan wanneer je wat en waar gaat publishen.
+  //Voor de communicatie tussen de puzzels, check "Datacommunicatie.docx". (terug tevinden in dezelfde repository) 
+  client.publish("Wristbands/reset","Reset wristbands");
+  digitalWrite(LED_PIN, HIGH);
+  delay(1000);
+  digitalWrite(LED_PIN, LOW);
+  delay(100000000000000000);
+  
+
+}
